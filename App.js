e702b2c193bdd467f5ba10795e7fccda02313a4d
Robin Alwert <Ralwert@outlook.com>
2023-09-22 09:51:21 +0200

    Mer bra text

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!Titta det appelsin</Text>
+      <Text>MEra text</Text>
       <StatusBar style="auto" />
     </View>
   );
